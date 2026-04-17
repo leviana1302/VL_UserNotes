0cce7f322de9aa85f8fb0df602cd55930332c2a1
Leviana <verena.lueschen@gmail.com>
2026-04-17 18:51:04 +0200

    20260417.5 git

---
+++ script.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         VL_UserNotes
+// @namespace    http://tampermonkey.net/
+// @version      20260417.5
+// @description  Beautify User Notes
+// @author       Verena
+// @match        https://www.geocaching.com/geocache/GC*
+// @match        https://www.geocaching.com/seek/cache_details.aspx*
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+    alert('Mein Skript lädt! 🎉');
+})();
